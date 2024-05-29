904e37b73f388e144d999f6e7ea74ad96dce0c38
Luis Felipe Brenes <luisfelipebrenes@gmail.com>
2024-05-29 13:12:29 -0600

    Personalizacion en registro

--- app.js
+++ app.js
@@ -30,12 +30,40 @@ app.use(session({
 
 //8
 const connection = require('./database/db');
+const { error } = require('console');
 
+//9 Rutas
 app.get('/', (req, res)=>{
-    res.render('index', {msg:'MENSAJE DESDE NODE'});
+    res.render('index', {msg:'Ejemplo 1'});
 })
 app.get('/login', (req, res)=>{
     res.render('login');
+})
+app.get('/register', (req, res)=>{
+    res.render('register');
+})
+
+//10 Registrar
+app.post('/register', async (req, res)=>{
+    const user = req.body.user;
+    const name = req.body.name;
+    const pass = req.body.pass;
+    let passwordHaash = await bcryptjs.hash(pass, 8);
+    connection.query('INSERT INTO users SET ?', {user:user, name:name, pass: passwordHaash}, async(error, results)=>{
+        if(error){
+            console.log(error);
+        }else{
+            res.render('register',{
+                alert: true,
+                alertTitle: "Registration",
+                alertMessage: "Successful Registration!",
+                alertIcon: "success",
+                showConfirmButton: false,
+                timer: 1500,
+                ruta: "/"
+            })
+        }
+    })
 })
 
 app.listen(3000, (req, res)=>{
